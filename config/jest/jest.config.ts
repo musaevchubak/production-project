07ba7e2436af230eb4ac4e647b23dfc9b69841cc
chubak <musaev4t@gmail.com>
2023-03-24 14:59:24 +0600

    upgrate sidebar, added button, add new word in translation

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -6,10 +6,10 @@
 import path from 'path';
 
 export default {
-    // All imported modules in your tests should be mocked automatically
+    // All imported modules in your test should be mocked automatically
     // automock: false,
 
-    // Stop running tests after `n` failures
+    // Stop running test after `n` failures
     // bail: 0,
 
     // The directory where Jest should store its cached dependency information
@@ -88,7 +88,7 @@ export default {
     // A set of global variables that need to be available in all test environments
     // globals: {},
 
-    // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+    // The maximum amount of workers used to run your test. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
     // An array of directory names to be searched recursively up from the requiring module's location
@@ -110,7 +110,7 @@ export default {
     // A preset that is used as a base for Jest's configuration
     // preset: undefined,
 
-    // Run tests from one or more projects
+    // Run test from one or more projects
     // projects: undefined,
 
     // Use this configuration option to add custom reporters to Jest
@@ -128,7 +128,7 @@ export default {
     // Automatically restore mock state and implementation before every test
     // restoreMocks: false,
 
-    // The root directory that Jest should scan for tests and modules within
+    // The root directory that Jest should scan for test and modules within
     // rootDir: undefined,
 
     // A list of paths to directories that Jest should use to search for files in
@@ -158,7 +158,7 @@ export default {
 
     // The glob patterns Jest uses to detect test files
 
-    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    // An array of regexp pattern strings that are matched against all test paths, matched test are skipped
     // testPathIgnorePatterns: [
     //   "/node_modules/"
     // ],
@@ -193,7 +193,7 @@ export default {
     // Indicates whether each individual test should be reported during the run
     // verbose: undefined,
 
-    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+    // An array of regexp patterns that are matched against all source file paths before re-running test in watch mode
     // watchPathIgnorePatterns: [],
 
     // Whether to use watchman for file crawling
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from './Button';
+import { Button, ButtonSize, ButtonTheme } from './Button';
 
 export default {
     title: 'shared/Button',
@@ -25,20 +25,73 @@ export const Clear = Template.bind({});
 
 Clear.args = {
     children: 'Text',
-    theme: ThemeButton.CLEAR,
+    theme: ButtonTheme.CLEAR,
 };
 
 export const Outline = Template.bind({});
 Outline.args = {
     children: 'Text',
-    theme: ThemeButton.OUTLINE,
+    theme: ButtonTheme.OUTLINE,
 };
 
-export const OutlineDark = Template.bind({});
-
-OutlineDark.args = {
+export const OutlineSizeL = Template.bind({});
+OutlineSizeL.args = {
     children: 'Text',
-    theme: ThemeButton.OUTLINE,
+    theme: ButtonTheme.OUTLINE,
+    size: ButtonSize.L,
 };
 
-OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+export const OutlineXl = Template.bind({});
+OutlineXl.args = {
+    children: 'Text',
+    theme: ButtonTheme.OUTLINE,
+};
+
+export const BackgroundTheme = Template.bind({});
+
+BackgroundTheme.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND,
+};
+
+export const BackgroundInverted = Template.bind({});
+
+BackgroundInverted.args = {
+    children: 'Text',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+};
+
+export const Square = Template.bind({});
+
+Square.args = {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    square: true,
+};
+
+export const SquareSizeL = Template.bind({});
+
+SquareSizeL.args = {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    size: ButtonSize.L,
+    square: true,
+};
+
+export const SquareSizeM = Template.bind({});
+
+SquareSizeM.args = {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    size: ButtonSize.M,
+    square: true,
+};
+
+export const SquareSizeXl = Template.bind({});
+
+SquareSizeXl.args = {
+    children: '>',
+    theme: ButtonTheme.BACKGROUND_INVERTED,
+    size: ButtonSize.XL,
+    square: true,
+};
