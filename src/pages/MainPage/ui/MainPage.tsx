96183e852e3abec9dcda47a1bffa756c8304be47
chubak <musaev4t@gmail.com>
2023-03-31 14:38:27 +0600

    update of Main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -10,7 +9,6 @@ const MainPage = () => {
         <div>
             <BugButton />
             {t('Главная страница')}
-            <Counter />
         </div>
     );
 };
