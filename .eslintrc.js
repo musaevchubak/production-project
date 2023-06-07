237a1589a9f79d22e5179cc0902a099cab47a1f0
chubak <musaev4t@gmail.com>
2023-06-07 15:22:35 +0600

    update custom eslint plugin, add support, for aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,7 +60,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'production-best-project-plugin/path-checker': 'error',
+        'production-best-project-plugin/path-checker': ['error', {alias: '@'}],
     },
     globals: {
         __IS_DEV__: true,
