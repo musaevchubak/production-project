5654f740f8c2bdb5d8697c118d49ba667d1d78d8
chubak <musaev4t@gmail.com>
2023-06-09 22:28:54 +0600

    add helpers for slice, selectors actions

--- src/entities/Counter/model/selectors/getCounterValue/getCounterValue.ts
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.ts
@@ -1,8 +1,4 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { getCounter } from '../getCounter/getCounter';
-import { CounterSchema } from '../../types/counterSchema';
+import { buildSelector } from '@/shared/lib/store';
 
-export const getCounterValue = createSelector(
-    getCounter,
-    (counter: CounterSchema) => counter.value,
-);
+// кортеж
+export const [useCounterValue, getCounterValue] = buildSelector((state) => state.counter.value);
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,36 +1,45 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/shared/ui/Button';
-import { counterActions } from '../model/slice/counterSlice';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
-    const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
     const { t } = useTranslation();
+    const { decrement, increment, add } = useCounterActions();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const handleInc = () => {
+        increment();
     };
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+    const handleDec = () => {
+        decrement();
+    };
+
+    const handleAdd = () => {
+        add(5);
     };
 
     return (
         <div>
             <h1 data-testid="value-title">{counterValue}</h1>
             <Button
-                onClick={increment}
+                onClick={handleInc}
                 data-testid="increment-btn"
             >
                 {t('increment')}
             </Button>
             <Button
                 data-testid="decrement-btn"
-                onClick={decrement}
+                onClick={handleDec}
             >
                 {t('decrement')}
+            </Button>
+            <Button
+                data-testid="decrement-btn"
+                onClick={handleAdd}
+            >
+                {t('add')}
             </Button>
         </div>
     );
