acbb11ff270011448e34d6484e6decb1c5689dc3
chubak <musaev4t@gmail.com>
2023-06-17 17:02:51 +0600

    skipp one test, add stubs fixtures

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -23,6 +23,14 @@ describe('Пользователь заходит на страницу стат
         cy.getByTestId('CommentCard.Content').should('have.length', 1);
     });
     it('И ставит оценку', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(4, 'feedback');
+        cy.get('[data-selected=true]').should('have.length', 4);
+    });
+    it('И ставит оценку (пример с стабом на фикстурах)', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
         cy.getByTestId('ArticleDetails.Info');
         cy.getByTestId('RatingCard').scrollIntoView();
         cy.setRate(4, 'feedback');
--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -4,8 +4,20 @@ describe('Пользователь заходит на страницу со с�
             cy.visit('articles');
         });
     });
-    it('и статьи успешно подгружаются', () => {
+    it.skip('и статьи успешно подгружаются', () => {
         cy.getByTestId('ArticleList').should('exist');
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+        cy.get('sdfsdf').should('exist');
+    });
+    it('На стабах (фикстурах)', () => {
+        cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+    });
+
+    it.skip('Пример заскипанного теста', () => {
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+        cy.get('asfasf').should('exist');
     });
 });
