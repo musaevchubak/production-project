8e3847804bf54cfde5ae6d1916d88ed0b27aafdb
chubak <musaev4t@gmail.com>
2023-06-17 21:31:03 +0600

    add feature flags concept

--- src/entities/User/model/types/user.ts
+++ src/entities/User/model/types/user.ts
@@ -1,10 +1,12 @@
 import { UserRole } from '../consts/userConsts';
+import { FeatureFlags } from '@/shared/types/featureFlags';
 
 export interface User {
     id: string;
     userName: string;
     avatar?: string;
     roles?: UserRole[];
+    features?: FeatureFlags;
 }
 
 export interface UserSchema {
