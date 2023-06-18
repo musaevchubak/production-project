e6793a3f665960626bfea7d7ae793add098d8a71
chubak <musaev4t@gmail.com>
2023-06-18 19:00:23 +0600

    add toggle features for components

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,6 +61,7 @@ module.exports = {
                     'direction',
                     'gap',
                     'border',
+                    'feature'
                 ],
             },
         ],
