59fc8677f5213de2eaa46a568f2850e382f62d2e
chubak <musaev4t@gmail.com>
2023-06-26 11:37:40 +0600

    add addons for input, button, Styled for input with pixel perfect

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
+import React, { ButtonHTMLAttributes, memo, ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
@@ -32,6 +32,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
      * Увеличивает кнопку на всю свободную ширину
      */
     fullWidth?: boolean;
+    addonLeft?: ReactNode;
+    addonRight?: ReactNode;
 }
 
 export const Button = memo((props: ButtonProps) => {
@@ -43,6 +45,8 @@ export const Button = memo((props: ButtonProps) => {
         disabled,
         fullWidth,
         size = 'm',
+        addonLeft,
+        addonRight,
         ...otherProps
     } = props;
 
@@ -50,6 +54,7 @@ export const Button = memo((props: ButtonProps) => {
         [cls.square]: square,
         [cls.disabled]: disabled,
         [cls.fullWidth]: fullWidth,
+        [cls.widthAddon]: Boolean(addonLeft) || Boolean(addonRight),
     };
 
     return (
@@ -63,7 +68,9 @@ export const Button = memo((props: ButtonProps) => {
             disabled={disabled}
             {...otherProps}
         >
+            <div className={cls.addonLeft}>{addonLeft}</div>
             {children}
+            <div className={cls.addonRight}>{addonRight}</div>
         </button>
     );
 });
