202b4b89c31103c1a8a8ad40d324df586e7491b5
chubak <musaev4t@gmail.com>
2023-06-27 11:43:06 +0600

    redesign article details, fix

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextAlign } from '@/shared/ui/deprecated/Text';
+import { Text as TextDeprecated, TextAlign } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
 import { ArticleImageBlock } from '../../model/types/article';
 import cls from './ArticleImageBlockComponent.module.scss';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface ArticleImageBlockComponentProps {
     className?: string;
@@ -22,7 +24,16 @@ export const ArticleImageBlockComponent = memo(
             >
                 <img src={block.src} alt={block.title} className={cls.img} />
                 {block.title && (
-                    <Text text={block.title} align={TextAlign.CENTER} />
+                    <ToggleFeatures
+                        feature="isAppRedesigned"
+                        on={<Text text={block.title} align="center" />}
+                        off={
+                            <TextDeprecated
+                                text={block.title}
+                                align={TextAlign.CENTER}
+                            />
+                        }
+                    />
                 )}
             </div>
         );
